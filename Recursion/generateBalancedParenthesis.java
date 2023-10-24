d6c3a8fd39d5f1f16922ef51879c64434f4e0f46
hiteshwarmehla21 <hiteshwar.mehla@gmail.com>
2023-10-24 20:02:43 +0530

    24/10/2023 - Recursion And Backtracking Question added

--- Recursion/generateBalancedParenthesis.java
+++ Recursion/generateBalancedParenthesis.java
@@ -5,16 +5,27 @@ import java.util.Vector;
 public class generateBalancedParenthesis {
     public static void main(String[] args) {
         int parenthesisCount = 3;
-        Vector<String> balancedParenthesis = solveParenthesis(parenthesisCount, parenthesisCount, new Vector<>());
-        System.out.println(balancedParenthesis);
+        Vector<String> balancedParenthesisDFS = solveParenthesisDFS(parenthesisCount, parenthesisCount, "", new Vector<>());
+        System.out.println(balancedParenthesisDFS);
     }
 
-    static Vector<String> solveParenthesis(int open, int close, Vector<String> result){
-        if(open == 0 && close ==0){
-
+    static Vector<String> solveParenthesisDFS(int open, int close, String possibleBalancedParenthesisDFS, Vector<String> result){
+        if(open == 0 && close == 0){
+            result.add(possibleBalancedParenthesisDFS);
         }
+
+        if(open>close || open<0 || close<0){
+            return result;
+        }
+
+        possibleBalancedParenthesisDFS += "{";
+        solveParenthesisDFS(open-1, close, possibleBalancedParenthesisDFS, result);
+        possibleBalancedParenthesisDFS = possibleBalancedParenthesisDFS.substring(0, possibleBalancedParenthesisDFS.length()-1);
+
+        possibleBalancedParenthesisDFS += "}";
+        solveParenthesisDFS(open, close-1, possibleBalancedParenthesisDFS, result);
+        possibleBalancedParenthesisDFS = possibleBalancedParenthesisDFS.substring(0, possibleBalancedParenthesisDFS.length()-1);
+
         return result;
     }
-
-
 }
