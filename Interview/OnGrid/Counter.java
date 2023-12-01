5e83c15453ea5079b9c34d9a9199cd60194e7e07
hiteshwarmehla21 <hiteshwar.mehla@gmail.com>
2023-12-01 12:49:58 +0530

    29/11/2023 - Design Patterns added

--- Interview/OnGrid/Counter.java
+++ Interview/OnGrid/Counter.java
@@ -24,4 +24,34 @@ class Counter {
             lock.unlock();
         }
     }
+
+    /*
+    ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+// Submit a Runnable task
+executorService.submit(() -> System.out.println("Executing Runnable"));
+
+// Submit a Callable task and retrieve the result using Future
+Future<String> future = executorService.submit(() -> {
+    System.out.println("Executing Callable");
+    return "Result";
+});
+
+try {
+    String result = future.get();
+    System.out.println("Result: " + result);
+} catch (InterruptedException | ExecutionException e) {
+    e.printStackTrace();
+}
+
+// Shutdown the executor service
+executorService.shutdown();
+
+Common implementations of ExecutorService include:
+ThreadPoolExecutor: A versatile and customizable implementation.
+ScheduledThreadPoolExecutor: Extends ThreadPoolExecutor to support task scheduling.
+Executors.newFixedThreadPool(int n): Creates a fixed-size thread pool.
+Executors.newCachedThreadPool(): Creates a dynamically sized thread pool.
+Executors.newSingleThreadExecutor(): Creates a single-threaded executor.
+     */
 }
